1c53fb945308248afc7e5e615dc4c15bbb545fd8
Bryan Pritchett <dasassygoat@coderbox.co>
2024-07-15 18:16:25 -0500

    Suggested changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 string testText = "This is some new text";
@@ -10,3 +10,5 @@ var teeHeeHee = "body count";
 string numb = "4";
 
 string numbNuttz = "some string";
+
+Console.WriteLine("First: " + numb + " Second: " + numbNuttz + " And original: " + teeHeeHee);
